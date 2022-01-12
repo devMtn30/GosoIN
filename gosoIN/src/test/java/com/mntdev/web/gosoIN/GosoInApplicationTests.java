581c62ed85b61f0f2437ced4fa895df49c564452
DESKTOP-N7R8PFD\김산 <devmtn30@gmail.com>
2022-01-12 22:24:57 +0900

    First Setting

---
+++ gosoIN/src/test/java/com/mntdev/web/gosoIN/GosoInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mntdev.web.gosoIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosoInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
